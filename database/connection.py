f5c765d40e6ae4ab5f3000e5bff80c83ebe0a97f
Deivyt G <dgarcia66383@universidadean.edu.co>
2025-04-10 12:11:35 -0500

    conexion a la nube

--- database/connection.py
+++ database/connection.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/hoomie_db"  # Cambia según tu usuario y contraseña
+DATABASE_URL = "mysql+pymysql://root:nyQKgoeRzwVQwXQWJpFJZGHknrbbKRdH@yamabiko.proxy.rlwy.net:39022/railway"  # Cambia según tu usuario y contraseña
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
