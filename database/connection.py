ac43c3ce8bc32104352230bae76f1258e23889f7
Deivyt G <116587097+Deivyt2608@users.noreply.github.com>
2025-11-11 16:34:44 -0500

    Change database connection to PostgreSQL
    Updated DATABASE_URL to use PostgreSQL instead of MySQL.

--- database/connection.py
+++ database/connection.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:WGUEuZYpIsEWIvpTRHQIzFTbkrXNBijb@shinkansen.proxy.rlwy.net:10299/railway"  # Cambia según tu usuario y contraseña
+DATABASE_URL = "postgreql://postgres:Santi124.@db.zvrpmagggxufaqcaitic.supabase.co:5432/postgres"  # Cambia según tu usuario y contraseña
 #DATABASE_URL = "mysql+pymysql://root:@localhost/hoomie"
-aaa
+
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
