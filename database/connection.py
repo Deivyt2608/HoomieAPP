4fca2b7c5570144ae20a27568be24e337a24ca27
Deivyt G <116587097+Deivyt2608@users.noreply.github.com>
2025-11-11 16:29:55 -0500

    Update database connection configuration
    Removed commented DATABASE_URL and added a new line.

--- database/connection.py
+++ database/connection.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "mysql+pymysql://root:WGUEuZYpIsEWIvpTRHQIzFTbkrXNBijb@shinkansen.proxy.rlwy.net:10299/railway"  # Cambia según tu usuario y contraseña
 #DATABASE_URL = "mysql+pymysql://root:@localhost/hoomie"
-
+aaa
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
